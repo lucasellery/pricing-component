08ab4e55cb81120b2e210ef80f9ae1815eeb822a
Your Name <olucasellery@gmail.com>
2021-01-31 14:18:24 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+function Header() {
+  return (
+    <div>
+      <h1 className={styles.h1}>Something</h1>
+    </div>
+  );
+}
+
+export default Header;
