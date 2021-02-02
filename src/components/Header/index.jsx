b0fe1803e77fec2a73ff938753ffb7b246c67305
Your Name <olucasellery@gmail.com>
2021-02-02 00:12:15 -0300

    (feat) Header completed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,21 @@
+import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import styles from "./styles.module.css";
 
 function Header() {
   return (
-    <div>
-      <h1 className={styles.h1}>Something</h1>
-    </div>
+    <>
+      <Grid container direction="column" alignItems="center" className={styles.headerContainer}>
+        <Grid item className={styles.firstTitleContainer}>
+          <Typography className={styles.firstTitle}>Simple, traffic-based pricing</Typography>
+        </Grid>
+
+        <Grid item justify="center" alignContent="center" className={styles.subtitleContainer}>
+          <Typography className={styles.subtitleOne}>Sign-up for our 30-day trial.</Typography>
+          <Typography className={styles.subtitleTwo}>No credit card required.</Typography>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,23 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
 import Header from "../../components/Header";
+import styles from "./styles.module.css";
 
 function Home() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Grid
+        container
+        direction="column"
+        // justify="flex-start"
+        alignItems="center"
+        className={styles.containerHome}
+      >
+        <Grid item xs={12} sm={12} md={12}>
+          <Header />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
