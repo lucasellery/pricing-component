e64924dc3176bebbe98191251a424311fa95b454
Your Name <olucasellery@gmail.com>
2021-02-06 15:20:49 -0300

    (featura) starting to do slider pricing component

--- src/pages/CardPricing/index.jsx
+++ src/pages/CardPricing/index.jsx
@@ -1,8 +1,18 @@
-import { Grid } from "@material-ui/core";
+import { Grid, Input, makeStyles, Slider } from "@material-ui/core";
+import { blue } from "@material-ui/core/colors";
 import React from "react";
 import styles from "./styles.module.css";
 
+const useStyles = makeStyles({
+  '.MuiSlider-thumb': {
+    background: blue,
+  }
+
+})
+
 function CardPricing() {
+  const styleRoot = useStyles();
+
   return (
     <>
       <Grid
@@ -11,13 +21,27 @@ function CardPricing() {
         direction="column"
         alignItems="center"
       >
-        <Grid item container>
-          <Grid item>
-            <p className={styles.firstLabel}>100k pageviews</p>
-          </Grid>
+        <Grid item>
+          <p className={styles.firstLabel}>100k pageviews</p>
         </Grid>
 
-        <Grid item container></Grid>
+        <Grid item className={styles.containerSlider}>
+          <Slider style={styleRoot} className={styles.slider} value={30} aria-labelledby="input-slider" />
+        </Grid>
+
+        <Grid item>
+          <Input 
+            value={''}
+            margin='none'
+            inputProps={{
+              step: 10,
+              min: 0,
+              max: 100,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
       </Grid>
     </>
   );
